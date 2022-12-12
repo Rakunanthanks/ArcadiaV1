46c495cd6c6aa9cee338a103330d185058c5588b
Happy\priya <bansalsre24@gmail.com>
2022-12-12 22:22:55 +0530

    Added page classes

---
+++ src/test/java/arcadia/pages/ComponentDB/HeaderPanel.java
@@ -0,0 +1,21 @@
+package arcadia.pages.ComponentDB;
+
+import arcadia.pages.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HeaderPanel extends BasePage {
+    public HeaderPanel(WebDriver driver) {
+        super(driver);
+    }
+    @FindBy(css = "button[value=\"Add New Component\"]") private WebElement addNewComponent;
+    @FindBy(id = "btnImport") private WebElement importCsv;
+    @FindBy(id = "idexport") private WebElement exportCsv;
+    @FindBy(id = "dataexport") private WebElement exportData;
+    @FindBy(id = "customExport") private WebElement exportCustomFields;
+    @FindBy(id = "btnaddsimilar") private WebElement addSimilar;
+    @FindBy(id = "btnmove") private WebElement btnCopy;
+    @FindBy(id = "btnDelete") private WebElement btnDelete;
+    @FindBy(css = "select[name=\"company\"]") private WebElement selectBoxCompany;
+}
