de6912cfadf5a4e45ddd21b19e6a4fc3f6ab4bcc
Rakunanthan <ragunanthan1@gmail.com>
2022-12-21 11:12:10 +0530

    Created Test Scenario for Bundle Tolerance

--- src/test/java/arcadia/stepdefinations/LoginStepDefinitions.java
+++ src/test/java/arcadia/stepdefinations/LoginStepDefinitions.java
@@ -169,6 +169,7 @@ public class LoginStepDefinitions {
     public void based_on_drawing_orchestrator_components_are_created() throws IOException, InterruptedException, AWTException {
         List<DrawingInstructor> drawingInstructorList =  new DrawingHelper().getDrawingInstruction(context.testIdentifier);
         new DrawingHelper().drawOrchestrator(drawingInstructorList,context.driver);
+
     }
 
 
