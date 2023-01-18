6f2734b7481ff20dca6f9dac12f61272b40c85f5
Rakunanthan <ragunanthan1@gmail.com>
2023-01-18 14:59:24 +0000

    Rebasing

--- src/test/java/arcadia/pages/LoginPage.java
+++ src/test/java/arcadia/pages/LoginPage.java
@@ -18,10 +18,8 @@ public class LoginPage extends BasePage{
     }
     public  void Login(){
         new WebDriverWait(driver, Duration.ofSeconds(2000)).until(ExpectedConditions.elementToBeClickable(userName));
-
         userName.sendKeys(System.getProperty("userName"));
         passWord.sendKeys(System.getProperty("password"));
-
         submitButton.click();
     }
 
