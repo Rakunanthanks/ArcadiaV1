87d58013ef5092ffde4fc3eb9bb45e4aebe042f2
Rakunanthan <ragunanthan1@gmail.com>
2023-01-19 11:30:28 +0000

    Fixed Rebasing Issue

--- src/test/java/arcadia/constants/EndPoint.java
+++ src/test/java/arcadia/constants/EndPoint.java
@@ -2,17 +2,13 @@ package arcadia.constants;
 
 public enum EndPoint {
     TRAINING("mercury1_21_1/"),
-    PROJECT("/index.lp?app=projects2&appoption=10&project=2&projName=quickstart"),
+    PROJECT("/index.lp?app=projects2&appoption=10&project=2&projName=projectName"),
     PROFILE("/index.lp??app=generaledit&profilecode=profileName"),
-    SLEEVETUBE("/index.lp?app=componentsv2&form=sleevetube&company=CADONIX&database=quickstart"),
-    HARNESSBUNDLEDISPLAY("/index.lp?app=generaledit&appoption=3&section=Bundle%20Default%20Display&mainsection=Harness&profilecode=quickstart"),
-
+    SLEEVETUBE("/index.lp?app=componentsv2&form=sleevetube&company=CADONIX&database=databaseName"),
+    HARNESSBUNDLEDISPLAY("/index.lp?app=generaledit&appoption=3&section=Bundle%20Default%20Display&mainsection=Harness&profilecode=profileName"),
     SETTINGS("/index.lp?menu=settings"),
-
-    COMPONENTDB("/index.lp?app=componentsv2&database=componentDB"),
-
+    COMPONENTDB("/index.lp?app=componentsv2&database=databaseName"),
     AutomationCompanyProfile("/index.lp?app=generaledit&profilecode=Automation"),
-
     BUNDLEDEFAULTDISPLAY("/index.lp?app=generaledit&appoption=3&section=Bundle%20Default%20Display&mainsection=Harness&profilecode=Automation"),
     TEST("/arcadia.lua?projectID=2&mode=0&taskID=HAR34&appmode=init&profile=Automation&newSession=TP0ShzKFZEHl&userName=ragu#{\"id\":\"1671551143277\",\"sheetID\":\"9d4bfa6c7bce11ed908802de8e05bcf8\"}");
     public final String url;
