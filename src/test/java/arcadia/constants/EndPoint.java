bdf5daabf25277ac84763fcaeca2f353dc5f156a
Rakunanthan <ragunanthan1@gmail.com>
2023-02-02 21:01:17 +0000

    Changed Endpoints

--- src/test/java/arcadia/constants/EndPoint.java
+++ src/test/java/arcadia/constants/EndPoint.java
@@ -9,8 +9,8 @@ public enum EndPoint {
     GENERALMACROS("/index.lp?app=generaledit&appoption=3&section=Macros&mainsection=General&profilecode=quickstart"),
     SETTINGS("/index.lp?menu=settings"),
     COMPONENTDB("/index.lp?app=componentsv2&database=databaseName"),
-    AutomationCompanyProfile("/index.lp?app=generaledit&profilecode=Automation"),
-    BUNDLEDEFAULTDISPLAY("/index.lp?app=generaledit&appoption=3&section=Bundle%20Default%20Display&mainsection=Harness&profilecode=Automation");
+    AutomationCompanyProfile("/index.lp?app=generaledit&profilecode=profileName"),
+    BUNDLEDEFAULTDISPLAY("/index.lp?app=generaledit&appoption=3&section=Bundle%20Default%20Display&mainsection=Harness&profilecode=profileName");
     public final String url;
     EndPoint(String url) {
         this.url = url;
