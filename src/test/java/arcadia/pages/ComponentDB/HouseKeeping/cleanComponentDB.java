0c0fea6fe6eb8ed0334656ce212338b570b7ac1d
Aravind Muthusamy <aravindmuthusamy@Aravinds-MacBook-Pro-2.local>
2023-02-05 00:02:03 +0000

    house keeping function

--- src/test/java/arcadia/pages/ComponentDB/HouseKeeping/cleanComponentDB.java
+++ src/test/java/arcadia/pages/ComponentDB/HouseKeeping/cleanComponentDB.java
@@ -1,6 +1,7 @@
 package arcadia.pages.ComponentDB.HouseKeeping;
 
 import arcadia.pages.BasePage;
+import arcadia.utils.SeleniumCustomCommand;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -10,10 +11,14 @@ public class cleanComponentDB  extends BasePage {
     public cleanComponentDB(WebDriver driver) {
         super(driver);
     }
-    public void deleteRecordsInComponentDB(WebElement element , String searchString ) throws InterruptedException {
+    public void deleteRecordsInComponentDB(String componentDBElementName , String searchString ) throws InterruptedException {
         boolean proceedWithDelete = true;
         while (proceedWithDelete) {
+            Thread.sleep(3000);
+            WebElement element = driver.findElement(By.cssSelector("input[placeholder=\""+componentDBElementName+"\"]"));
+            new SeleniumCustomCommand().waitForElementVisibility(driver,element);
             element.sendKeys(searchString);
+            Thread.sleep(1000);
             element.sendKeys(Keys.ENTER);
             Thread.sleep(1000);
             boolean noRecordPresentMessageDisplayed = driver.findElements(By.cssSelector("#allcomponents >tbody >tr[class=\"no-records-found\"]")).size() > 0;
@@ -26,12 +31,15 @@ public class cleanComponentDB  extends BasePage {
                 WebElement delete = driver.findElement(By.cssSelector("button[name=\"delete\"]"));
                 delete.click();
                 WebElement deleteConfirm = driver.findElement(By.cssSelector("button[data-bb-handler=\"confirm\"]"));
+                new SeleniumCustomCommand().waitForElementVisibility(driver,deleteConfirm);
                 deleteConfirm.click();
+                Thread.sleep(2000);
                 for (int i = 0; i < 50; i++) {
                     boolean isModelPresent = driver.findElements(By.cssSelector("div[class=\"bootbox-body\"]")).size() >0;
                     if(isModelPresent){
                         WebElement deleteSuccessBodyOk = driver.findElement(By.cssSelector("button[data-bb-handler=\"ok\"]"));
                         deleteSuccessBodyOk.click();
+                        Thread.sleep(3000);
                         break;
                     }
                     Thread.sleep(3000);
@@ -41,12 +49,10 @@ public class cleanComponentDB  extends BasePage {
     }
 
     public void initiateComponentDBHouseKeeping() throws InterruptedException {
-        WebElement companyElement = driver.findElement(By.cssSelector("input[placeholder=\"Company\"]"));
-        deleteRecordsInComponentDB(companyElement,"testcompany");
+        deleteRecordsInComponentDB("Company","testcompany");
         driver.navigate().refresh();
         Thread.sleep(1000);
-        WebElement descriptionElement = driver.findElement(By.cssSelector("input[placeholder=\"Description\"]"));
-        deleteRecordsInComponentDB(descriptionElement,"testdescription-");
+        deleteRecordsInComponentDB("Description","testdescription-");
     }
 
 
