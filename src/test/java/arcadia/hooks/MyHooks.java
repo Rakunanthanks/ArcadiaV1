c52437f622580ec2ed9e5d1a6a935eacd35a5fcd
Aravind Muthusamy <aravindmuthusamy@Aravinds-MacBook-Pro-2.local>
2023-02-12 20:48:16 +0000

    hook changes

--- src/test/java/arcadia/hooks/MyHooks.java
+++ src/test/java/arcadia/hooks/MyHooks.java
@@ -80,7 +80,7 @@ public class MyHooks {
         dbName="";
         connectorID = "";
         terminalImagePath = "";
-        driver.close();
         driver.quit();
+        driver = null;
     }
 }
