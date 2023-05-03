fa2ae82f5cbd7df55af86d6c48b703da721aa806
vipula <vipula@adobe.com>
2023-05-03 09:27:56 +0530

    Fixes for number format exception

--- src/test/java/arcadia/pages/CreateSchematic.java
+++ src/test/java/arcadia/pages/CreateSchematic.java
@@ -15,7 +15,6 @@ public class CreateSchematic extends BasePage {
     @FindBy(css = "input[name=\"Worktask\"]") private WebElement schematicWorkTask;
     @FindBy(css = "input[name=\"Title\"]") private WebElement schematicTitle;
     @FindBy(css = "input[name=\"Description\"]") private WebElement schematicDescription;
-    @FindBy(css = "input[name=\"Part Number\"]") private WebElement schematicPartNumber;
     @FindBy(css = "//label[text()='Part Number']/parent::div//input") private WebElement harnessPartNumber;
 
     @FindBy(xpath = "//input[@name=\"Part Number\"] | //select[@name=\"Part Number\"]//following-sibling::div//input") private WebElement schematicPartNumber;
